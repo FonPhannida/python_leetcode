2198b43e1a286371ed4b2650f24aa7f6323da388
fonphannida <jphan1@hotmail.com>
2025-03-06 18:02:51 +0700

    add 2 tasks

---
+++ basic/arithmetic-operator.py
@@ -0,0 +1,14 @@
+print("Input a and b: ")
+a = int(input().strip())
+print("a: ", a)
+b = int(input().strip())
+print("b: ", b)
+
+sum_number = a+b
+diff_number = a-b
+product = a*b
+
+print("sum of the two numbers: ", sum_number)
+print("sum of the two numbers: ", diff_number)
+print("sum of the two numbers: ", product)
+
