f13d5fdb952c5dff2ad7232159ef5e18ad5fc52f
fonphannida <jphan1@hotmail.com>
2025-03-08 22:03:59 +0700

    loops & function

---
+++ basic/loops.py
@@ -0,0 +1,7 @@
+#The list of non-negative integers that are less than  is . Print the square of each number on a separate
+print("Please input  n: ")
+n = int(input().strip())
+
+for i in range(n):
+    print(i*i)
+
